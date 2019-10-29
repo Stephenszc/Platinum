7f86019426a6a9485c1d23032de08902ca45236b
Stephenszc <32018798+Stephenszc@users.noreply.github.com>
2019-10-29 11:45:55 -0400

    Add files via upload

---
+++ scripts/dsplabels.py
@@ -0,0 +1,123 @@
+class1_1 = ['R','K','E','D','Q','N']
+class1_2 = ['G','A','S','T','P','H','Y']
+class1_3 = ['C','L','V','I','M','F','W']
+hydrophobicity = [class1_1, class1_2, class1_3]
+
+class2_1 = ['G','A','S','T','P','D']
+class2_2 = ['N','V','E','Q','I','L']
+class2_3 = ['M','H','K','F','R','Y','W']
+vanDerWaalsVolume = [class2_1, class2_2, class2_3]
+
+class3_1 = ['L','I','F','W','C','M','V','Y']
+class3_2 = ['P','A','T','G','S']
+class3_3 = ['H','Q','R','K','N','E','D']
+polarity = [class3_1, class3_2, class3_3]
+
+class4_1 = ['G','A','S','D','T']
+class4_2 = ['C','P','N','V','E','Q','I','L']
+class4_3 = ['K','M','H','F','R','Y','W']
+polarizability = [class4_1, class4_2, class4_3]
+
+class5_1 = ['K','R']
+class5_2 = ['A','N','C','Q','G','H','I','L','M','F','P','S','T','W','Y','V']
+class5_3 = ['D','E']
+charge = [class5_1, class5_2, class5_3]
+
+class6_1 = ['E','A','L','M','Q','K','R','H']
+class6_2 = ['V','I','Y','C','W','F','T']
+class6_3 = ['G','N','P','S','D']
+secondaryStructure = [class6_1, class6_2, class6_3]
+
+class7_1 = ['A','L','F','C','G','I','V','W']
+class7_2 = ['P','K','Q','E','N','D']
+class7_3 = ['M','P','S','T','H','Y']
+solventAccessibility = [class7_1, class7_2, class7_3]
+
+class8_1 = ['G','Q','D','N','A','H','R']
+class8_2 = ['K','T','S','E','C']
+class8_3 = ['I','L','M','F','P','W','Y','V']
+surfaceTension = [class8_1, class8_2, class8_3]
+
+class9_1 = ['D','H','I','K','N','P','R','W','Y']
+class9_2 = ['E','Q','S','T','G','A','M','F']
+class9_3 = ['C','L','V']
+proteinBogan = [class9_1, class9_2, class9_3]
+
+class10_1 = ['C','D','F','M','P','Q','R','W','Y']
+class10_2 = ['A','G','H','V','L','N','S','T']
+class10_3 = ['E','I','K']
+proteinMa = [class10_1, class10_2, class10_3]
+
+class11_1 = ['G','K','N','Q','R','S','T','Y']
+class11_2 = ['A','D','E','F','H','I','L','V','W']
+class11_3 = ['C','M','P']
+proteinSchneider = [class11_1, class11_2, class11_3]
+
+class12_1 = ['G','H','K','N','Q','R','S','T','Y']
+class12_2 = ['A','D','E','F','I','P','V','W']
+class12_3 = ['C','L','M']
+proteinAhmad = [class12_1, class12_2, class12_3]
+
+class13_1 = ['H','K','M','R','Y']
+class13_2 = ['F','G','I','L','N','P','Q','S','V','W']
+class13_3 = ['C','D','E','A','T']
+proteinKim = [class13_1, class13_2, class13_3]
+
+class14_1 = ['H','G','K','M','R','S','Y','W']
+class14_2 = ['A','F','I','N','P','Q','T']
+class14_3 = ['C','D','E','L','V']
+proteinEllis = [class14_1, class14_2, class14_3]
+
+class15_1 = ['H','K','M','Q','R','S']
+class15_2 = ['A','D','E','F','G','L','N','P','V','Y']
+class15_3 = ['C','I','T','W']
+proteinPhipps = [class15_1, class15_2, class15_3]
+
+class16_1 = ['C','F','H','W','Y']
+class16_2 = ['G','I','L','N','M','S','T','R']
+class16_3 = ['A','E','D','K','P','Q','V']
+proteinKhazanov = [class16_1, class16_2, class16_3]
+
+class17_1 = ['C','F','H','W','Y','M']
+class17_2 = ['D','G','I','L','N','S','T','V']
+class17_3 = ['A','E','K','P','Q','R']
+proteinValidKhazanov = [class17_1, class17_2, class17_3]
+
+class18_1 = ['D','E','H','R','Y']
+class18_2 = ['C','F','K','M','N','Q','S','T','W']
+class18_3 = ['A','G','I','L','P','V']
+proteinImai = [class18_1, class18_2, class18_3]
+
+class19_1 = ['A','G','S']
+class19_2 = ['C','D','E','H','I','K','L','M','N','Q','P','T','V']
+class19_3 = ['F','R','W','Y']
+molecularWeight = [class19_1, class19_2, class19_3]
+
+class20_1 = ['R','K','D','N','E','Q','H']
+class20_2 = ['P','Y','S','T','G','A','C','V']
+class20_3 = ['W','M','F','L','I']
+cLogP = [class20_1, class20_2, class20_3]
+
+class21_1 = ['H','K','N','Q','R']
+class21_2 = ['D','E','S','T','W','Y']
+class21_3 = ['A','C','G','F','I','L','M','P','V']
+hydrogenBondDonor = [class21_1, class21_2, class21_3]
+
+class22_1 = ['D','E','H','N','Q','R']
+class22_2 = ['K','S','T','W','Y']
+class22_3 = ['A','C','G','F','I','L','M','P','V']
+hydrogenBondAcceptor = [class22_1, class22_2, class22_3]
+
+class23_1 = ['A','C','G','K','R','T']
+class23_2 = ['E','F','H','I','L','M','N','P','Q','S','V','W']
+class23_3 = ['D','Y']
+solubility = [class23_1, class23_2, class23_3]
+
+class24_1 = ['E','G','K','N','Q','S']
+class24_2 = ['A','D','H','I','P','R','T','V']
+class24_3 = ['C','F','L','M','W','Y']
+flexibility = [class24_1, class24_2, class24_3]
+
+
+
+

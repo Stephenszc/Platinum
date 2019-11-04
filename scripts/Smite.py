62479790ec881fc5fe9ca7e364646a736e0a8982
oladelemi <lordthebash@gmail.com>
2019-11-03 23:49:21 -0500

    Updated smite.py script

--- scripts/Smite.py
+++ scripts/Smite.py
@@ -4,21 +4,26 @@ Created on Thu Oct 31 11:11:27 2019
 
 @author: Zachary
 """
+#The idea is to be able to run smote on all our data yet smote only 
+# each type of protein using only data from its type
+# This will require we add the class (type ) of protein as another column
+#for identification 
+
 import pandas as pd
 import numpy as np
 import random
 
-basefile = pd.read_csv(r'DRNAseparated.csv')
+basefile = pd.read_csv(r'72featNoLabels.csv')
 output = []
-for i in range(1,23):
+for i in range(1,8795):
     pt1 = basefile.iloc[i-1:i,:]
     pt1 = np.array(pt1).flatten()
-    for j in range(i,23):
+    for j in range(i,8795):
         smoteOut = []
-        if i != j:
+        if i != j & basefile.iloc[i,73] == basefile.iloc[j,73]
             pt2 = basefile.iloc[j-1:j,:]
             pt2 = np.array(pt2).flatten()
-            for k in range(0,len(pt2)):
+            for k in range(0,len(pt2)-1):
                 pt1k = round(pt1[k] * 100000)
                 pt2k = round(pt2[k] * 100000)
                 if pt1k > pt2k:
@@ -29,6 +34,7 @@ for i in range(1,23):
                     rand = pt1k
                 rand = rand / 100000
                 smoteOut.append(rand)
+            smoutOut.append(basefile.iloc[j,73]) #Adds protein's type name to last column of 'smotted' values 
                 
             output.append(smoteOut)
             
